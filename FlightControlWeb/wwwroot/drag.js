2f78cca97c501b77d91975c932f8f8014e90365e
GuyStep <44813835+GuyStep@users.noreply.github.com>
2020-05-31 18:24:56 +0300

    JSON fields existance validation

--- FlightControlWeb/wwwroot/drag.js
+++ FlightControlWeb/wwwroot/drag.js
@@ -17,6 +17,47 @@ function readJSON() {
     let jdata;
     reader.onload = function () {
         jdata = reader.result.replace('/r', '');
+        let parsedJSON;
+        let validFile = true;
+        try {
+            JSON.parse(jdata);
+            parsedJSON = JSON.stringify(jdata);
+        } catch (e) {
+            showError("File is not a valid JSON");
+            return;
+        }
+        if (parsedJSON != null) {
+
+
+            if (!parsedJSON.includes("company_name")) {
+                validFile = false;
+                showError("Missing company name");
+            }
+            if (!parsedJSON.includes("date_time")) {
+                validFile = false;
+                showError("Missing date time");
+            }
+            if (!parsedJSON.includes("longitude")) {
+                validFile = false;
+                showError("Missing longitude");
+            }
+            if (!parsedJSON.includes("latitude")) {
+                validFile = false;
+                showError("Missing latitude");
+            }
+            if (!parsedJSON.includes("segments")) {
+                validFile = false;
+                showError("Missing segments");
+            }
+            if (!parsedJSON.includes("timespan_seconds")) {
+                validFile = false;
+                showError("Missing timespan seconds");
+            }
+            if (!validFile) {
+                return;
+            }
+        }
+        
         postData(jdata);
     }
     reader.readAsText(inputFile);
